9f9ebfdaf2ba27e82891d09879231499f1dc1b68
asedova <21244226+asedova@users.noreply.github.com>
2023-07-17 21:54:32 -0400

    Update parallel_plots.m
    small typo found

--- deepmd/scripts/parallel_plots.m
+++ deepmd/scripts/parallel_plots.m
@@ -2,7 +2,7 @@ function parallel_plots(filename)
 %filename is a string for a .csv file that is in your matlab path
 R=readtable(filename);
 %edit below for different combinations, this creates parallel plot in paper
-T=removevars(R,{'job','generation','uuid','birth_id','start_eval_time','stop_eval_time'});
+T=removevars(R,{'job','generation','uuid','birth_id','start_eval_time','stop_eval_time','eval_time'});
 
 energy=table2array(T(:, 8));force=table2array(T(:,9));
 
