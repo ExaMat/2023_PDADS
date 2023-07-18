1f12165ed781f217cad4ca38155ced793ec5e7b7
asedova <21244226+asedova@users.noreply.github.com>
2023-07-17 21:25:35 -0400

    Add files via upload
    Matlab script for parallel plots, Figure 3.

---
+++ deepmd/scripts/parallel_plots.m
@@ -0,0 +1,20 @@
+function parallel_plots(filename)
+%filename is a string for a .csv file that is in your matlab path
+R=readtable(filename);
+%edit below for different combinations, this creates parallel plot in paper
+T=removevars(R,{'job','generation','uuid','birth_id','start_eval_time','stop_eval_time'});
+
+energy=table2array(T(:, 8));force=table2array(T(:,9));
+% binEdgesForce = [0 0.05 0.6];binEdgesEnergy = [0 0.004 0.01];
+% bins_force={'good forces', 'bad forces'};
+% bins_energy={'good energy', 'bad energy'};
+
+a=energy<0.004;b=force<0.04;both_l=a&b;
+accuracy = categorical(both_l,[0 1],{'inaccurate' 'accurate'});
+Both_T=addvars(T,accuracy);
+
+start=table2array(R(:, 12));stop=table2array(R(:, 13));
+runtime=stop-start;runtime=runtime/60;
+time_T=addvars(Both_T,runtime);
+ 
+figure;p_time=parallelplot(time_T,'GroupVariable', 'accuracy','Color', {'[0.7 0.7 0.7]','[0.3 0.6 1]'},'FontSize',16);p_time.LineAlpha = [0.3 0.3];
